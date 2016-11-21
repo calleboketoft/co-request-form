c9c43480f28dd68a9d3e66531f312442183c2594
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-11-21 10:11:22 +0100

    updated dependencies

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-gulp.task('sass', function () {
-  gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/css'))
-})
