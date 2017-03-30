98b193fb937bd1a13daaeb29b095e91e8296bff0
Carl-Ola Boketoft <calleboketoft@gmail.com>
2017-03-30 12:10:41 +0200

    updated to angular v4

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var request_form_module_1 = require("./src/request-form.module");
 exports.RequestFormModule = request_form_module_1.RequestFormModule;
 var request_form_component_1 = require("./src/request-form.component");
